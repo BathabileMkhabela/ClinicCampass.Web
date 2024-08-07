ca691ce5771471207b9eacfc5eb989ea3261036a
JonathanManaka <mphomanaka91@gmil.com>
2024-08-07 12:15:36 +0200

    completd back button

---
+++ src/features/admin/components/patients_waiting_visit_comp/back_button_comp.jsx
@@ -0,0 +1,8 @@
+import BackIconComp from "./back_icon_comp";
+export default function BackButton(){
+    return(
+        <>
+            <button type="button"><li>{<BackIconComp/>}</li>Back</button>
+        </>
+    );
+}
--- src/features/admin/components/patients_waiting_visit_comp/body.jsx
+++ src/features/admin/components/patients_waiting_visit_comp/body.jsx
@@ -2,6 +2,9 @@ import H1titleComponent from "../../../components/h1_title_component";
 import "../../../../styles/admin_styles.css"
 import UrgencyLevelComp from "./urgency_level_component";
 import PatientsListTableComp from "./patients_list_table";
+import BackButton from "./back_button_comp";
+import BackIconComp from "./back_icon_comp";
+
 export default function BodyComponent(){
     return(
         <div className="patients_list_container">
@@ -14,6 +17,7 @@ export default function BodyComponent(){
             <div className="patients_list_table_container">
                 <PatientsListTableComp/>
             </div>
+              <div className="back_btn">{<BackButton/>}</div>
         </div>
     );
 }
